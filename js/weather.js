39fa4f787c653bbd2026ea976469abd0bc451784
Soop <75235831+msb741852@users.noreply.github.com>
2022-05-13 14:24:48 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,8 +5,7 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    
-    console.log(url);
+
     fetch(url).then(response => response.json()).then(data => {
         const city = document.querySelector("#weather span:first-child");
         const temp = document.querySelector("#weather span:nth-child(2)");
@@ -14,8 +13,6 @@ function onGeoOk(position) {
 
         city.innerText = data.name;
         temp.innerText = `${Math.round(data.main.temp)}°C`;
-        // http://openweathermap.org/img/wn/10d@2x.png
-        // temp.innerText = `${data.weather[0].icon}`
         weather.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     });
 }
